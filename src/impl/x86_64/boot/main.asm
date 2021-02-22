e024a009127264d7a6f56b906fe334bb3e2590af
Peter Sysel <peter@sysel.at>
2021-02-22 22:13:17 +0100

    HE ENK

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -4,5 +4,7 @@ section .text
 bits 32
 start:
     ; print "OK"
-    mov dword [0xb8000], 0x2f4b2f4f
+    mov dword [0xb8000], 0x2f452f48
+    mov dword [0xb8004], 0x2f452f20
+    mov dword [0xb8008], 0x2f4b2f4e
     hlt
